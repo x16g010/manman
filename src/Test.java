c2411210bb8ddcf6da912d1d82b5c2ecfed9d87e
WIN_NT\x16g010 <samejima@gmail.com>
2018-05-31 14:43:10 +0900

    おう

--- src/Test.java
+++ src/Test.java
@@ -2,7 +2,10 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Gitテスト");
+        System.out.println("追加");
+        System.out.println("追加2");
     }
-
 }
+
+
 
